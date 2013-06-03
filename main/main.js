a8df9f9ee1e92b8b983e56090872f3aa1d1af3e3
cormac <cormac@studiorua.ie>
2013-06-03 18:29:45 +0100

    added documentation for all files

--- main/main.js
+++ main/main.js
@@ -2,15 +2,14 @@
 'use strict';
 (function(window, document, undefined) {
   define(
-    ['jquery', 'lib/twitter_search', 'lib/SearchBox',
-    'lib/ResultsRouter', 'backbone', 'lib/ResultsCollection'
+    ['jquery', 'lib/twitter_search', 'lib/input',
+    'lib/router', 'backbone', 'lib/twitter_stream'
     ],
-    function($, TwitterSearch, SearchBox, ResultsRouter, Backbone, ResultsModule) {
-      console.log(SearchBox.views);
+    function($, TwitterSearch, Input, AppRouter, Backbone, TwitterStream) {
       var ts = new TwitterSearch();
-      var sb = new SearchBox.views.SearchView();
-      ResultsModule.init();
-      var router = new ResultsRouter();
+      var sb = new Input.views.SearchView();
+      TwitterStream.init();
+      var router = new AppRouter();
       Backbone.history.start();
   });
 }(window, document));
