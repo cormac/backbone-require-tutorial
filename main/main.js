a763da8f518439a008d285f2033032ae70ef7873
cormac <cormac@studiorua.ie>
2013-06-03 19:32:46 +0100

    router complete

--- main/main.js
+++ main/main.js
@@ -8,7 +8,6 @@
     function($, TwitterSearch, Input, AppRouter, Backbone, TwitterStream) {
       var ts = new TwitterSearch();
       var sb = new Input.views.SearchView();
-      TwitterStream.init();
       var router = new AppRouter();
       Backbone.history.start();
   });
