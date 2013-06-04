143574c4d932bf496da90829757f3f06ac6aec85
cormac <cormac@studiorua.ie>
2013-06-04 02:01:13 +0100

    removed dependencies from main

--- main/main.js
+++ main/main.js
@@ -1,11 +1,6 @@
 /*global window, document, define */
 'use strict';
-(function(window, document, undefined) {
-  define(
-    ['jquery', 'lib/twitter_search', 'lib/input',
-    'lib/router', 'backbone', 'lib/twitter_stream'
-    ],
-    function($, TwitterSearch, Input, AppRouter, Backbone, TwitterStream) {
-      var ts = new TwitterSearch();
-  });
-}(window, document));
+define(
+  [],
+  function() {
+});
