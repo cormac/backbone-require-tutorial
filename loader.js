641e486d1c6ea76f9574d9c24c46f1d2f7e72559
cormac <cormac@studiorua.ie>
2013-06-04 10:08:46 +0100

    added protocol to allow running the app form file:

--- loader.js
+++ loader.js
@@ -5,9 +5,9 @@
     "paths": {
       "lib": "lib",
       "main": "main",
-      "jquery": "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
-      "backbone": "//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min",
-      "underscore":"//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min"
+      "jquery": "http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
+      "backbone": "http://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min",
+      "underscore":"http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min"
     },
     shim: {
       underscore: {
